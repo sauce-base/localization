4192e3fc96d1597a8a5f2a0562bfe53c89019435
roble <renanroble@gmail.com>
2025-09-24 18:50:16 +0100

    test: clean up and moving auth tests from core to the module dir

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-uses(Tests\TestCase::class);
-
-it('returns a successful response for the Module URL', function () {
-    $response = $this->get('/locale/en');
-
-    $response->assertStatus(200);
-});
--- tests/Unit/ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-uses(Tests\TestCase::class);
-
-test('that true is true', function () {
-    expect(true)->toBeTrue();
-});
